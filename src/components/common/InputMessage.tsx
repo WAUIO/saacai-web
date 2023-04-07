e0cc281542d9bf913d7fb193049134b3d521e7f1
Stenny <stenny.manao@gmail.com>
2023-04-07 16:51:44 +0300

    position of in^put

--- src/components/common/InputMessage.tsx
+++ src/components/common/InputMessage.tsx
@@ -1,10 +1,10 @@
 export const InputMessage = () => {
     return (
-        <div className="flex">
+        <div className="flex flex-row">
             <img src="images/man.jpg" alt="user" className="w-10"/>
-            <div className="ml-2">
-                <div>Message</div>
-                <div>00:00</div>
+            <div className="ml-2 flex flex-col justify-between">
+                <div className="font-normal">Message</div>
+                <div className="font-lighter text-xs">00:00</div>
             </div>
         </div>
     )
