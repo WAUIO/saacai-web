2bf76d3d12db231c95b6e30c044de12e3bf03358
Stenny <stenny.manao@gmail.com>
2023-04-07 16:58:18 +0300

    position of input

--- src/components/common/InputMessage.tsx
+++ src/components/common/InputMessage.tsx
@@ -1,10 +1,12 @@
 export const InputMessage = () => {
     return (
         <div className="flex flex-row">
-            <img src="images/man.jpg" alt="user" className="w-10"/>
+            <img src="images/man.jpg" alt="user" className="w-10 h-10 object-cover"/>
             <div className="ml-2 flex flex-col justify-between">
-                <div className="font-normal">Message</div>
-                <div className="font-lighter text-xs">00:00</div>
+                <div className="font-normal max-w-xs text-left">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis pariatur repellendus magnam illum eligendi eos nostrum maiores consectetur. Tenetur ut eligendi quas earum aut quidem magnam minima dolorem sapiente quam.
+                </div>
+                <div className="font-lighter text-xs text-right">00:00</div>
             </div>
         </div>
     )
